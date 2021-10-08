84eb0b3ea35e7653348cf58d79581ea9b22dd754
Danyl Timofeyev <timofeev.danil@gmail.com>
2021-10-09 01:23:22 +0300

    Initial Commit

---
+++ CleanSwiftCombineTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftCombineTemplate
+//
+//  Created by Danyl Timofeyev on 09.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
